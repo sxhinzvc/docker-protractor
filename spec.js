bd85bb11c60ab393df8674746868dc60f34d07e1
Julia Lee <julee@pivotal.io>
2019-11-08 10:20:27 -0600

    fixes failing test

--- spec.js
+++ spec.js
@@ -2,6 +2,6 @@ describe('Protractor Demo App', function() {
   it('should have a title', function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
 
-    expect(browser.getTitle()).toEqual(' Calculator');
+    expect(browser.getTitle()).toEqual('Super Calculator');
   });
 });
